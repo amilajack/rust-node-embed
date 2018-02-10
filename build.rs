839c3708a9c503a18cad246f8544260082eb72b2
Amila Welihinda <amilajack@gmail.com>
2018-02-10 15:01:11 -0800

    Initial commit

---
+++ build.rs
@@ -0,0 +1,28 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("./node-v9.5.0/include/node/node.h")
+        .clang_arg("-L.")
+        .clang_arg("-lnode")
+        .clang_arg("-I./node-v9.5.0/include/node")
+        .clang_args(["-x", "c++", "-std=c++11"].iter())
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
